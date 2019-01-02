16a2bac9ac4b12309ba3ac976aae25dafdf99e3d
ratnakumarpaleru <37299060+ratnakumarpaleru@users.noreply.github.com>
2019-01-02 09:30:27 -0500

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+//import { DateRangeComponent } from './date-range/date-range.component';
+//import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
+import { HttpClientModule } from '@angular/common/http'; 
+import { NgxPaginationModule } from 'ngx-pagination';
+import { DatepickerModule, BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //DateRangeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    //OwlDateTimeModule,
+    //OwlNativeDateTimeModule,
+    HttpClientModule,
+    NgxPaginationModule,
+    BsDatepickerModule.forRoot(),
+    DatepickerModule.forRoot() 
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+//ng build --deploy-url https://incytedev.sharepoint.com/sites/EHSMMS/SiteAssets/ApplicationCode/Reports/Nonresolved/ --base-href https://incytedev.sharepoint.com/sites/EHSMMS/Pages/Non-Resolved.aspx
